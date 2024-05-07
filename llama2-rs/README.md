799c01ce9aa644dd5f7ab854a1c4db7f6ee9c8b0
jumzzz <jummel19@gmail.com>
2024-05-08 02:24:24 +0800

    made clap work

--- llama2-rs/README.md
+++ llama2-rs/README.md
@@ -7,10 +7,9 @@
 ##### Constraints
 - Use `memmap2` crate since Karpathy's implementation uses memmap in raw C.
 - Use `clap` crate for command-line interface instead of default Rust cli from standard library.
-- 
 
 #### Main Parts
-- The parameter/configuration part
+- The `Configuration` part
 - The `Transformer` part
 - The `Tokenizer` part
 - The `Sampler` part
--- llama2-rs/src/main.rs
+++ llama2-rs/src/main.rs
@@ -1,3 +1,44 @@
+use clap::{Parser,ValueEnum};
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
+enum Mode {
+    Generate,
+    Chat,
+}
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Config {
+    #[arg(long, required = true)]
+    checkpoint_path: String,
+    #[arg(long, default_value = "tokenizer.bin")]
+    tokenizer_path: String,
+    #[arg(long, default_value_t = 1.0)]
+    temperature: f32,
+    #[arg(long, default_value_t = 0.9)]
+    topp: f32,
+    #[arg(long, default_value_t = 256)]
+    steps: u32,
+    #[arg(long, default_value = "")]
+    prompt: String,
+    #[arg(long, default_value_t = 0)]
+    rng_seed: u64,
+    #[arg(value_enum, default_value_t = Mode::Generate)]
+    mode: Mode,
+    #[arg(long, default_value = "")]
+    system_prompt: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let config = Config::parse();
+
+    println!("Checkpoint Path: {}", config.checkpoint_path);
+    println!("Tokenizer Path: {}", config.tokenizer_path);
+    println!("Temperature: {}", config.temperature);
+    println!("Top-p: {}", config.topp);
+    println!("Steps: {}", config.steps);
+    println!("Prompt: '{}'", config.prompt);
+    println!("RNG Seed: {}", config.rng_seed);
+    println!("Mode: {:?}", config.mode);
+    println!("System Prompt: '{}'", config.system_prompt);
 }
