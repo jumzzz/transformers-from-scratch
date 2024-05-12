5bacd18b61c33d193738e06dbd84f2b9dd921efb
jumzzz <jummel19@gmail.com>
2024-05-12 12:45:18 +0800

    refactor

--- llama2-rs/src/main.rs
+++ llama2-rs/src/main.rs
@@ -1,4 +1,5 @@
 use memmap2::{Mmap, MmapOptions};
+use std::ops::Index;
 use std::slice;
 use std::fs::File;
 use std::io;
@@ -46,26 +47,37 @@ impl Config {
     }
 }
 
-
-struct TransformerWeights <'a>{
-    token_embedding_table   : &'a [f32], 
-    rms_att_weight          : &'a [f32],
-    rms_ffn_weight          : &'a [f32],
-    wq                      : &'a [f32],
-    wk                      : &'a [f32],
-    wv                      : &'a [f32],
-    wo                      : &'a [f32],
-    w1                      : &'a [f32],
-    w2                      : &'a [f32],
-    w3                      : &'a [f32],
-    rms_final_weight        : &'a [f32],
-    wcls                    : &'a [f32],
+struct IndexRange {
+    start: usize,
+    end: usize,
 }
 
+impl IndexRange {
+    fn new(start: usize, end: usize) -> IndexRange {
+        IndexRange {
+            start: start,
+            end: end,
+        }
+    }
+}
 
-impl<'a> TransformerWeights <'a> {
-    fn load(data: &'a Mmap, cfg: &Config, max_index: usize) -> Self {
+struct TransformerIndexRange {
+    token_embedding_table   : IndexRange,
+    rms_att_weight          : IndexRange, 
+    rms_ffn_weight          : IndexRange,
+    wq                      : IndexRange,
+    wk                      : IndexRange,
+    wv                      : IndexRange,
+    wo                      : IndexRange,
+    w1                      : IndexRange,
+    w2                      : IndexRange,
+    w3                      : IndexRange,
+    rms_final_weight        : IndexRange,
+    wcls                    : IndexRange,
+}
 
+impl TransformerIndexRange {
+    fn new(cfg: &Config, max_index: usize) -> TransformerIndexRange {
         let head_size = cfg.dim / cfg.n_heads;
 
         let start_tok_emb_tbl = CONFIG_SIZE_IN_BYTES;
@@ -108,31 +120,112 @@ impl<'a> TransformerWeights <'a> {
         let start_wcls = end_freq_cis_img;
         let end_wcls = max_index;
 
-        let map_bytes_to_f32 = |start_index: usize, end_index: usize| -> &'a [f32] {
+        TransformerIndexRange {
+            token_embedding_table: IndexRange::new(start_tok_emb_tbl, end_tok_emb_tbl), 
+            rms_att_weight: IndexRange::new(start_rms_att_weight, end_rms_att_weight), 
+            rms_ffn_weight: IndexRange::new(start_rms_ffn_weight, end_rms_ffn_weight),
+            wq: IndexRange::new(start_wq, end_wq),
+            wk: IndexRange::new(start_wk, end_wk),
+            wv: IndexRange::new(start_wv, end_wv),
+            wo: IndexRange::new(start_wo, end_wo),
+            w1: IndexRange::new(start_w1, end_w1),
+            w2: IndexRange::new(start_w2, end_w2),
+            w3: IndexRange::new(start_w3, end_w3),
+            rms_final_weight: IndexRange::new(start_rms_final_weight, end_rms_final_weight),
+            wcls: IndexRange::new(start_wcls, end_wcls),
+        } 
+    }
+}
+
+struct TransformerWeights <'a>{
+    token_embedding_table   : &'a [f32], 
+    rms_att_weight          : &'a [f32],
+    rms_ffn_weight          : &'a [f32],
+    wq                      : &'a [f32],
+    wk                      : &'a [f32],
+    wv                      : &'a [f32],
+    wo                      : &'a [f32],
+    w1                      : &'a [f32],
+    w2                      : &'a [f32],
+    w3                      : &'a [f32],
+    rms_final_weight        : &'a [f32],
+    wcls                    : &'a [f32],
+}
+
+
+impl<'a> TransformerWeights <'a> {
+    fn load(data: &'a Mmap, cfg: &Config, max_index: usize) -> Self {
+
+        // let head_size = cfg.dim / cfg.n_heads;
+
+        // let start_tok_emb_tbl = CONFIG_SIZE_IN_BYTES;
+        // let end_tok_emb_tbl = start_tok_emb_tbl + cfg.vocab_size * cfg.dim * FLOAT_SIZE;
+
+        // let start_rms_att_weight = end_tok_emb_tbl;
+        // let end_rms_att_weight = start_rms_att_weight + cfg.n_layers * cfg.dim * FLOAT_SIZE;
+
+        // let start_wq = end_rms_att_weight;
+        // let end_wq = start_wq + cfg.n_layers * cfg.dim * cfg.n_heads * head_size * FLOAT_SIZE;
+
+        // let start_wk = end_wq;
+        // let end_wk = start_wk + cfg.n_layers * cfg.dim * cfg.n_kv_heads * head_size * FLOAT_SIZE;
+
+        // let start_wv = end_wk;
+        // let end_wv = start_wv + cfg.n_layers * cfg.dim * cfg.n_kv_heads * head_size * FLOAT_SIZE;
+
+        // let start_wo = end_wv;
+        // let end_wo = start_wo + cfg.n_layers * cfg.n_heads * head_size * cfg.dim * FLOAT_SIZE;
+
+        // let start_rms_ffn_weight = end_wo;
+        // let end_rms_ffn_weight = start_rms_ffn_weight + cfg.n_layers * cfg.dim * FLOAT_SIZE;
+
+        // let start_w1 = end_rms_ffn_weight;
+        // let end_w1 = start_w1 + cfg.n_layers * cfg.dim * cfg.hidden_dim * FLOAT_SIZE;
+
+        // let start_w2 = end_w1;
+        // let end_w2 = start_w2 + cfg.n_layers * cfg.hidden_dim * cfg.dim * FLOAT_SIZE;
+
+        // let start_w3 = end_w2;
+        // let end_w3 = start_w3 + cfg.n_layers * cfg.dim * cfg.hidden_dim * FLOAT_SIZE;
+
+        // let start_rms_final_weight = end_w3;
+        // let end_rms_final_weight = start_rms_final_weight + cfg.dim * FLOAT_SIZE;  // Skipping rms_final_weight for now
+
+        // let start_freq_cis_real = end_rms_final_weight;
+        // let end_freq_cis_real = start_freq_cis_real + cfg.seq_len * head_size * FLOAT_SIZE / 2; // Skip freq_cis_real
+        // let start_freq_cis_img = end_freq_cis_real;
+        // let end_freq_cis_img = start_freq_cis_img + cfg.seq_len * head_size * FLOAT_SIZE / 2; // Skip freq_cis_imag
+        // let start_wcls = end_freq_cis_img;
+        // let end_wcls = max_index;
+        let index_ranges = TransformerIndexRange::new(cfg, max_index);
+
+        let map_bytes_to_f32 = |index_range: &IndexRange| -> &'a [f32] {
             unsafe {
+                let start_index = index_range.start;
+                let end_index = index_range.end;
                 slice::from_raw_parts(data[start_index..end_index].as_ptr() as *const f32, (end_index - start_index) / FLOAT_SIZE)
             }
         };
         
         let shared_weights = if cfg.vocab_size > 0 { true } else { false };
         let wcls = if shared_weights {
-            map_bytes_to_f32(start_tok_emb_tbl, end_tok_emb_tbl)
+            map_bytes_to_f32(&index_ranges.token_embedding_table)
         } else {
-            map_bytes_to_f32(start_wcls, end_wcls)
+            map_bytes_to_f32(&index_ranges.wcls)
         };
 
         TransformerWeights {
-            token_embedding_table: map_bytes_to_f32(start_tok_emb_tbl, end_tok_emb_tbl), 
-            rms_att_weight: map_bytes_to_f32(start_rms_att_weight, end_rms_att_weight), 
-            rms_ffn_weight: map_bytes_to_f32(start_rms_ffn_weight, end_rms_ffn_weight), 
-            wq: map_bytes_to_f32(start_wq, end_wq), 
-            wk: map_bytes_to_f32(start_wk, end_wk), 
-            wv: map_bytes_to_f32(start_wv, end_wv), 
-            wo: map_bytes_to_f32(start_wo, end_wo), 
-            w1: map_bytes_to_f32(start_w1, end_w1), 
-            w2: map_bytes_to_f32(start_w2, end_w2), 
-            w3: map_bytes_to_f32(start_w3, end_w3), 
-            rms_final_weight: map_bytes_to_f32(start_rms_final_weight, end_rms_final_weight), 
+            token_embedding_table: map_bytes_to_f32(&index_ranges.token_embedding_table), 
+            rms_att_weight: map_bytes_to_f32(&index_ranges.rms_att_weight), 
+            rms_ffn_weight: map_bytes_to_f32(&index_ranges.rms_ffn_weight), 
+            wq: map_bytes_to_f32(&index_ranges.wq), 
+            wk: map_bytes_to_f32(&index_ranges.wk), 
+            wv: map_bytes_to_f32(&index_ranges.wv), 
+            wo: map_bytes_to_f32(&index_ranges.wo), 
+            w1: map_bytes_to_f32(&index_ranges.w1), 
+            w2: map_bytes_to_f32(&index_ranges.w2), 
+            w3: map_bytes_to_f32(&index_ranges.w3), 
+            rms_final_weight: map_bytes_to_f32(&index_ranges.rms_final_weight), 
             wcls: wcls,       
         } 
     }
