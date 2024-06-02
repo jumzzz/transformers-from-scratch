b10b4123ce6f0bb353e4ed9e640c7c2b838368b8
jumzzz <jummel19@gmail.com>
2024-06-02 10:44:40 +0800

    yes

--- scratch-code/bpe/bpe.py
+++ scratch-code/bpe/bpe.py
@@ -7,6 +7,11 @@ tokenizer = AutoTokenizer.from_pretrained("gpt2")
 def print_json(data):
     print(json.dumps(data, indent=2))
 
+def print_line_by_line(data):
+    for key,val in data.items():
+        print(f"{key} = {val}")
+    print("")
+
 corpus = [
     "This is the Hugging Face Course.",
     "This chapter is about tokenization.",
@@ -21,16 +26,8 @@ for cnt, text in enumerate(corpus, start=1):
     words_with_offsets = tokenizer.backend_tokenizer.pre_tokenizer.pre_tokenize_str(text)
     new_words = [word for word, offset in words_with_offsets]
     
-##    print(f"[{cnt}/{total_corpus}] words_with_offsets = {words_with_offsets}")
-##    print(f"[{cnt}/{total_corpus}] new_words = {new_words}")
-##    print("")
-
     for word in new_words:
         word_freqs[word] += 1
-
-# print_json(dict(word_freqs))
-
-# The next step is to compute the base vocabulary, formed by all the characters used in the corpus
 
 alphabet = []
 for word in word_freqs.keys():
@@ -45,12 +42,20 @@ splits = {word: [c for c in word] for word in word_freqs.keys()}
 
 def compute_pair_freqs(splits):
     pair_freqs = defaultdict(int)
+    print("computing pairs:")
     for word, freq in word_freqs.items():
         split = splits[word]
+        print(f"word = {word}, splits[{word}] = {split}")
         if len(split) == 1:
             continue
         for i in range(len(split) - 1):
             pair = (split[i], split[i + 1])
             pair_freqs[pair] += freq
+    
+    print("")
     return pair_freqs
 
+pair_freqs = compute_pair_freqs(splits)
+
+print("pair_freqs:")
+print_line_by_line(pair_freqs)
